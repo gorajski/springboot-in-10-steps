0b6812319aa1295f85f4e2fa4b0829fdb3a7f7da
Nick Gorajski <nick_gorajski@bcbsil.com>
2020-02-13 20:43:02 -0600

    Add junit test

--- spring-in-steps/src/test/java/com/gorajski/spring/basics/springinsteps/SpringInStepsBasicApplicationTests.java
+++ spring-in-steps/src/test/java/com/gorajski/spring/basics/springinsteps/SpringInStepsBasicApplicationTests.java
@@ -1,9 +1,12 @@
 package com.gorajski.spring.basics.springinsteps;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+//import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+//@SpringBootTest
+@RunWith(SpringRunner.class)
 class SpringInStepsBasicApplicationTests {
 
 	@Test
---
+++ spring-in-steps/src/test/java/com/gorajski/spring/basics/springinsteps/basic/BinarySearchImplTest.java
@@ -0,0 +1,28 @@
+package com.gorajski.spring.basics.springinsteps.basic;
+
+import com.gorajski.spring.basics.springinsteps.SpringInStepsBasicApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+@ContextConfiguration(classes = SpringInStepsBasicApplication.class)    //defines the context
+@RunWith(SpringRunner.class)    //runs the context
+public class BinarySearchImplTest {
+
+//    @Mock
+
+    @Autowired
+    BinarySearchImpl binarySearch;
+
+    @Test
+    public void testBasicScenario() throws Exception git {
+        int result = binarySearch.binarySearch(new int[]{}, 5);
+        assertEquals(3, result);
+    }
+
+}
