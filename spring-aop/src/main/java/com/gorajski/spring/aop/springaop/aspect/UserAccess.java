143454d0d3aa10980dcbe4b47c3875d328f3e9f4
Nick Gorajski <nick_gorajski@bcbsil.com>
2020-02-18 21:13:00 -0600

    Add definitions for AOP terminology

--- spring-aop/src/main/java/com/gorajski/spring/aop/springaop/aspect/UserAccess.java
+++ spring-aop/src/main/java/com/gorajski/spring/aop/springaop/aspect/UserAccess.java
@@ -14,13 +14,22 @@ public class UserAccess {
     private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     // What kind of method calls would I want to intercept:
+
+
+    @Before("execution(* com.gorajski.spring.aop.springaop.data.*.*(..))")
     // execution(* PACKAGE.*.*(..))
     //         execution(*      <- here * means any method irrespective of return type
-    //         PACKAGE.*        <- any class within the specified PACKAGE
+    //         PACKAGE..*       <- any class of any SUBPACKAGE within the specified PACKAGE
     //         .*               <- all of that classes methods
     //         (..)             <- irrespective of their arguments
 
-    @Before("execution(* com.gorajski.spring.aop.springaop.business.*.*(..))")
+    // POINT CUT - the expression which defines what kind of methods you want to intercept.
+    // ADVICE - what you should do when the action is intercepted.
+    // POINT CUT + ADVICE = Aspect.
+    // JOIN POINT - a specific execution instance of an interception.
+    // WEAVING - the action of the AOP framework inserting your Aspect is being executed.
+    // WEAVER - the AOP framework.
+
     public void before(JoinPoint joinPoint) {
         //What to do?
         logger.info("Check for user access ");
